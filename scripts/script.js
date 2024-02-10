029c23e86a2ced93239970044620f9e6db1158f0
alexisfrancisco <amfrancisco2001@gmail.com>
2024-02-10 17:15:50 -0500

    added transition to take card out of envelope

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,10 @@
 let click = 0;
 
 function rotateCard() {
-        if (click == 0) {
-            var img = document.getElementById("card")
-            img.setAttribute("class", "rotated-card")
-        }
-        else {
-            img.style.width = "450px";
-            img.style.height = "300px";
-        }
-    click++;
-}
+    var img = document.getElementById("card");
+    img.setAttribute("class", "rotated-card");
+};
+
+// function closeDiv(divToClose) {
+//     divToClose.parentNode.style.display = "none";
+// };
