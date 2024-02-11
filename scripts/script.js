b1f25468fbbf4c55432836cab14d05762eeb56dd
alexisfrancisco <amfrancisco2001@gmail.com>
2024-02-11 00:22:43 -0500

    back of card flip fixed, added instructions message

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,25 @@
 let click = 0;
 
-function rotateCard() {
-    var img = document.getElementById("card");
-    img.setAttribute("class", "rotated-card");
+function openCard() {
+    var card = document.getElementById("card")
+    card.setAttribute("class", "card-opened");
 };
 
-// function closeDiv(divToClose) {
-//     divToClose.parentNode.style.display = "none";
-// };
+const card = document.querySelector(".card-opened");
+
+
+function flipCard() {
+    const card = document.querySelector(".card-opened");
+    card.classList.toggle("show");
+};
+
+
+function clicked() {
+    if (click == 0) {
+        openCard();
+        click++;
+    }
+    else {
+        flipCard()
+    }
+};
